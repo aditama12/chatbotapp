d8a57213d797779d910b943de08e61be09194762
masadiit <m.adiitaama@gmail.com>
2026-06-22 14:31:47 +0700

    perbaikan cors lagi

--- config/cors.php
+++ config/cors.php
@@ -5,7 +5,11 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173', 'http://127.0.0.1:5173', 'https://sakti-delta.vercel.app'],
+    'allowed_origins' => [
+        'http://localhost:5173',
+        'http://127.0.0.1:5173',
+        'https://sakti-delta.vercel.app' // 👈 TAMBAHKAN URL VERCEL KAMU DI SINI
+    ],
 
     'allowed_origins_patterns' => [],
 
